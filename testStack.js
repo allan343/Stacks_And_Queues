a20a0cff69cb454cc7eaf09c28c5612e92d21a28
Allan Cheng <allanhfcheng@gmail.com>
2020-06-30 21:21:30 -0700

    palindrome

--- testStack.js
+++ testStack.js
@@ -1,5 +1,28 @@
 var Stack = require('./Stack.js');
 
+function isPalindrome(inputString)
+{
+    let stack = new Stack();
+for (let i = 0; i < inputString.length; i++) {
+    stack.push(inputString.charAt(i));
+}
+
+let reverseString = "";
+
+while (!stack.isEmpty()) {
+    reverseString = reverseString+stack.pop();
+}
+
+if (inputString === reverseString)
+{
+    console.log("The input String is a palindrome.");
+    return true;
+}
+else
+{
+   console.log("The input String is not a palindrome.");
+}
+}
 function stackStuff(){
     let emptyStack = new Stack();
     let starTrek = new Stack();
@@ -12,6 +35,7 @@ function stackStuff(){
     console.log(starTrek.isEmpty());
     console.log(emptyStack.isEmpty());
     starTrek.display();
+    console.log("it is "+isPalindrome("allan")+" that it is a palindrome");
 }
 
 stackStuff();
