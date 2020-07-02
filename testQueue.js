b196bdafaa90f3b85d5d04901dbb9b43573640d5
Allan Cheng <allanhfcheng@gmail.com>
2020-07-01 23:54:11 -0700

    Queue

--- testQueue.js
+++ testQueue.js
@@ -6,9 +6,8 @@ function displayQueue(ls){
   
     
     while(currNode.next!==null){
-        //console.log(currNode.head.value);
-        currNode=currNode.next;
         console.log(currNode.value);
+        currNode=currNode.next;
     }
     
     }
@@ -24,15 +23,21 @@ function QueueStuff(){
     starTrekQ.enqueue("Uhura");
     starTrekQ.enqueue("Sulu");
     starTrekQ.enqueue("Checkov");
-  //  displayQueue(starTrekQ);
+
+    displayQueue(starTrekQ);
   console.log("peek method");
   console.log(starTrekQ.peek().value);
   // console.log(starTrekQ);
 console.log(starTrekQ.isEmpty());
   console.log(empty.isEmpty());
-
+  displayQueue(starTrekQ);
+  console.log("first dequeue");
+    starTrekQ.dequeue();
+    displayQueue(starTrekQ);
+    console.log("second dequeue");
+    starTrekQ.dequeue();
+    displayQueue(starTrekQ);
 }
-
 
 
 QueueStuff();
